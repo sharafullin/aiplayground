be874a10c3fefd048a34f694d98f196abf84e185
Andrei Sharafullin <sharafullin@mac.home>
2025-10-23 23:02:09 +0200

    init

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+module "avm-res-resources-resourcegroup" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = "0.2.1"
+
+  name = "rg-test"
+  location = var.location
+}
---
+++ terraform/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_version = "~> 1.9"
+    required_providers {
+
+    }
+}
+
+provider "azurerm" {
+    features {
+
+    }
+    subscription_id = "534f3f2d-a4b5-4218-96ad-1b90beb94e1b"
+}
