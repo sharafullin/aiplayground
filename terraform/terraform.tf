e8da84a4a1031de66748cfd01a6e091d81ea51e9
Andrei Sharafullin <sharafullin@mac.home>
2025-10-24 00:33:34 +0200

    init

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -10,5 +10,7 @@ provider "azurerm" {
 
     }
     enable-oidc: true
+    client_id       = "a8eb6959-65bd-46a7-ab16-67a02fc7663f"
     subscription_id = "534f3f2d-a4b5-4218-96ad-1b90beb94e1b"
+    tenant_id       = "b8afe45d-a365-4f0a-837a-6493cdc22154"
 }
