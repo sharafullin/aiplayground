ab0ba6bfa0e2f72773a42eeea7d3df7e2a13ae45
Andrei Sharafullin <sharafullin@mac.home>
2025-10-24 01:12:46 +0200

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,11 @@ module "avm-res-resources-resourcegroup" {
   name = "rg-test"
   location = var.location
 }
+
+module "avm-res-resources-resourcegroup" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = "0.2.1"
+
+  name = "rg-test2"
+  location = var.location
+}
